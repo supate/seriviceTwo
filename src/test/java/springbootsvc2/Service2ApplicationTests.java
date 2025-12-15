a4abcddd81fac85624ccda71a2ad7a52380a656c
supate <32972552+supate@users.noreply.github.com>
2025-12-15 10:22:17 +0530

    Update Service2ApplicationTests.java
    added comment in test class

--- src/test/java/springbootsvc2/Service2ApplicationTests.java
+++ src/test/java/springbootsvc2/Service2ApplicationTests.java
@@ -2,7 +2,9 @@ package springbootsvc2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+/*
+ * This is Test file
+ */
 @SpringBootTest
 class Service2ApplicationTests {
 
